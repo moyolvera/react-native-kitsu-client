905aa55e581b245b444042814eac24a5d167643b
Moises Olvera <moises.olvera@itexico.com>
2020-08-08 13:05:07 -0500

    Added async storage module

--- src/declaration/global.td.ts
+++ src/declaration/global.td.ts
@@ -1,6 +1,9 @@
 import { AxiosRequestState } from '../modules/AxiosRequestReducer';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AnyType = any;
+
 export type AxiosRequestResult<T> = {
   state: AxiosRequestState<T>;
-  triggerRequest: () => Promise<void>;
+  triggerRequest: (params?: string) => Promise<void>;
 };
